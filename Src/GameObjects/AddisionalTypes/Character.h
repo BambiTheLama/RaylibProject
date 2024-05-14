ad737935ab2d601e116aba8f735461e12dafbdb8
BambiTheLama <92307273+BambiTheLama@users.noreply.github.com>
2024-05-14 23:22:30 +0200

    Types

---
+++ Src/GameObjects/AddisionalTypes/Character.h
@@ -0,0 +1,13 @@
+//
+// Created by Szymek on 14.05.2024.
+//
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+class Character{
+public:
+    void move(float x,float y,float deltaTime);
+}
+
+#endif //CHARACTER_H
