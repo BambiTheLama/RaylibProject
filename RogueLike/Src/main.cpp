124f441853200984379d7bd2a31439e8a515820a
BambiTheLama <92307273+BambiTheLama@users.noreply.github.com>
2024-07-04 22:24:58 +0200

    update

--- RogueLike/Src/main.cpp
+++ RogueLike/Src/main.cpp
@@ -1,26 +1,7 @@
 #include "Core/Engine.h"
-#ifdef test
-#include "json.hpp"
-#include <fstream>
-#endif // test
-
-
 
 int main()
 {
-#ifdef test
-    const char* path = "Res/Weapon.json";
-    std::ifstream r(path);
-    if (r.is_open())
-    {
-        nlohmann::json j;
-        r >> j;
-        r.close();
-        std::ofstream w(path);
-        w << j.dump(2);
-        w.close();
-    }
-#endif // test
     Engine e;
     return 0;
 }
